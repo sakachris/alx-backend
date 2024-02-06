f6827d6d951e499900c085ad2792b818d6dd1c88
sakachris <sakachris90@mail.com>
2024-02-06 07:59:58 +0300

    setting up flask

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+''' 0-app.py '''
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app: Flask = Flask(__name__)
+babel: Babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index() -> str:
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
