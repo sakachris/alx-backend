d48675067c1355cf2054c9e18754a1843066e36b
sakachris <sakachris90@mail.com>
2024-02-07 05:29:14 +0300

    mock

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python3
-''' 0-app.py '''
+# 5-app.py
 
-from flask import Flask, render_template, request, g
-from flask_babel import Babel
-from typing import Union, Dict
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, _
 
 app: Flask = Flask(__name__)
 babel: Babel = Babel(app)
-# app = Flask(__name__)
-# babel = Babel(app)
 
-
-class Config:
-    ''' language configuration '''
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
-users = {
+# Mock user table
+users: dict = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
@@ -28,35 +15,30 @@ users = {
 }
 
 
-def get_user() -> Union[Dict, None]:
-    ''' retrieves a user '''
-    login_id = request.args.get('login_as')
-    if login_id:
-        return users.get(int(login_id))
-    return None
+def get_user(user_id: int) -> dict:
+    ''' getting user '''
+    return users.get(user_id)
 
 
 @app.before_request
 def before_request() -> None:
     ''' before request '''
-    user = get_user()
-    g.user = user
-
-
-@babel.localeselector
-def get_locale() -> str:
-    ''' determine the best match with our supported languages '''
-    locale_param = request.args.get('locale', '')
-    if locale_param in app.config["LANGUAGES"]:
-        return locale_param
-
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    user_id: int = int(request.args.get('login_as', 0))
+    g.user: dict = get_user(user_id) if user_id else None
 
 
 @app.route('/')
 def index() -> str:
     ''' index page '''
-    return render_template('5-index.html')
+    if g.user:
+        welcome_message: str = (
+                _(
+                    "You are logged in as %(username)s."
+                ) % {'username': g.user['name']}
+        )
+    else:
+        welcome_message: str = _("You are not logged in.")
+    return render_template('5-index.html', welcome_message=welcome_message)
 
 
 if __name__ == '__main__':
